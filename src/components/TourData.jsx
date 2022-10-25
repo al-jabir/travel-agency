0df426d13af7a0c66cc57958ade82a761566b25d
al-jabir <aljabir098@gmail.com>
2022-10-25 21:27:40 +0600

    done

---
+++ src/components/TourData.jsx
@@ -0,0 +1,30 @@
+import tour1 from '../assets/tour1.png';
+import tour2 from '../assets/tour2.png';
+import tour3 from '../assets/tour3.png3';
+
+const TourData = [
+  {
+    id: 1,
+    img: tour1,
+    title: `Santorini, Oia Greece`,
+    price: 2000,
+    reviews: `5K reviews`,
+  },
+
+  {
+    id: 2,
+    img: tour2,
+    title: `Lighthouse, Bellwood`,
+    price: 4000,
+    reviews: `5K reviews`,
+  },
+  {
+    id: 3,
+    img: tour3,
+    title: `Riverfront, Japan`,
+    price: 3000,
+    reviews: `5K reviews`,
+  },
+];
+
+export default TourData;
