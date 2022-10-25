a22e2071f065f5fd5b9cfab2dbec8b79e9e403df
al-jabir <aljabir098@gmail.com>
2022-10-25 19:14:51 +0600

    I have complete components

---
+++ src/components/ServiceData.jsx
@@ -0,0 +1,37 @@
+import service1 from '../assets/service1.png';
+import service2 from '../assets/service2.png';
+import service3 from '../assets/service3.png';
+import service4 from '../assets/service4.png';
+
+const ServiceData = [
+  {
+    id: 1,
+    img: service1,
+    title: `Choose Destination`,
+    des: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate,
+    iusto.`,
+  },
+  {
+    id: 2,
+    img: service2,
+    title: `Explore the Place`,
+    des: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate,
+    iusto.`,
+  },
+  {
+    id: 3,
+    img: service3,
+    title: `Start your journey`,
+    des: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate,
+    iusto.`,
+  },
+  {
+    id: 4,
+    img: service4,
+    title: `Let's Enjoy`,
+    des: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate,
+    iusto.`,
+  },
+];
+
+export default ServiceData;
---
+++ src/components/Services.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from 'styled-components';
+import ServiceData from './ServiceData';
+
+const Services = () => {
+  return (
+    <Section id='service'>
+      <div className='services'>
+        {ServiceData.map(({ id, img, title, des }) => {
+          return (
+            <div className='service' key={id}>
+              <img src={img} alt='card-service' />
+              <h3>{title}</h3>
+              <p>{des}</p>
+            </div>
+          );
+        })}
+      </div>
+    </Section>
+  );
+};
+
+export default Services;
+
+const Section = styled.section`
+  margin: 8rem 2rem;
+  .services {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 1rem;
+    .service {
+      padding: 1.5rem 2rem;
+      text-align: center;
+      background: var(--cardGrey);
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      gap: 1rem;
+      h3 {
+        color: var(--primaryText);
+      }
+      p {
+        color: var(--secondaryText);
+      }
+      img {
+        width: 128px;
+        height: 128px;
+      }
+      transition: var(--defaultTransition);
+      &:hover {
+        background: white;
+        box-shadow: 0px 7px 29px 0px rgba(100, 100, 111, 0.2);
+      }
+    }
+  }
+`;
