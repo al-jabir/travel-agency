09fec2fd5595b5f853dd0abd7ca8a1bab654906a
al-jabir <aljabir098@gmail.com>
2022-10-25 19:15:21 +0600

    initial code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import Destination from './components/Destination';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import Services from './components/Services';
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Home />
+      <Services />
+      <Destination />
     </>
   );
 };
