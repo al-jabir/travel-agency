7be0e7c52262b6b92f8caeb2f1e62dd30e0c31c8
al-jabir <aljabir098@gmail.com>
2022-10-25 19:28:43 +0600

    Added data json

---
+++ src/components/OfferData.jsx
@@ -0,0 +1,22 @@
+import { BiWorld } from 'react-icons/bi';
+import { BsHeadphones, BsPerson } from 'react-icons/bs';
+
+const OfferData = [
+  {
+    id: 1,
+    icon: <BsPerson />,
+    color: 'red',
+  },
+  {
+    id: 2,
+    icon: <BiWorld />,
+    color: 'green',
+  },
+  {
+    id: 3,
+    icon: <BsHeadphones />,
+    color: 'yellow',
+  },
+];
+
+export default OfferData;
