f52c9ee21bddb2a7d4bf23a8903ab5130746e48c
al-jabir <aljabir098@gmail.com>
2022-10-25 19:37:27 +0600

    Added text data json

--- src/components/OfferData.jsx
+++ src/components/OfferData.jsx
@@ -5,16 +5,19 @@ const OfferData = [
   {
     id: 1,
     icon: <BsPerson />,
+    text: `Best Travel Guide Always for your Services`,
     color: 'red',
   },
   {
     id: 2,
     icon: <BiWorld />,
+    text: `World Class Service Provide For You`,
     color: 'green',
   },
   {
     id: 3,
     icon: <BsHeadphones />,
+    text: `24/7 Strong Customer Support`,
     color: 'yellow',
   },
 ];
